6f174d2eeaeaa02aa66cc52d4ddc2286ab9a53a3
Derek Norrbom <dnorrbom@hakkasan.com>
2024-03-12 17:37:29 -0700

    update purchase details handler
    record the sale and email delivery status in dynamodb

--- amplify/functions/getPurchaseDetails/handler.ts
+++ amplify/functions/getPurchaseDetails/handler.ts
@@ -1,13 +1,13 @@
 // getPurchaseDetails/handler.ts
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import Stripe from 'stripe';
-import { Lambda } from 'aws-sdk';
-
-const lambda = new Lambda();
+import { Lambda, DynamoDB } from 'aws-sdk';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2023-10-16',
 });
+const lambda = new Lambda();
+const dynamoDb = new DynamoDB.DocumentClient();
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { sessionId } = event.queryStringParameters!;
@@ -31,20 +31,56 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       productPrice: paymentIntent.metadata.productPrice,
     };
 
-    // log customer email and purchase details
-    console.log('Customer email:', session.customer_details?.email);
-    console.log('Purchase details:', purchaseDetails);
-
-    // Invoke the sendPurchaseEmail Lambda function
-    await lambda
-      .invoke({
-        FunctionName: `${process.env.SEND_EMAIL_LAMBDA_NAME!}`,
-        Payload: JSON.stringify({
-          email: session.customer_details?.email,
-          ...purchaseDetails,
-        }),
+    // Check if a purchase record already exists for the given transaction ID
+    const existingPurchase = await dynamoDb
+      .get({
+        TableName: 'Purchase-7v5may5tl5bulonzl23ui4akcy-NONE',
+        Key: { id: paymentIntent.id },
       })
       .promise();
+
+    if (!existingPurchase.Item || !existingPurchase.Item.emailSent) {
+      // Create a new purchase record or update the existing one
+      const purchase = {
+        id : paymentIntent.id,
+        product: { id: purchaseDetails.productId },
+        buyer: session.customer_details?.email,
+        buyerEmail: session.customer_details?.email,
+        amount: paymentIntent.amount,
+        fee: paymentIntent.application_fee_amount,
+        stripeChargeId: paymentIntent.id,
+        createdAt: new Date().toISOString(),
+        emailSent: false, // Set the initial value to false
+      };
+
+      await dynamoDb
+        .put({
+          TableName: 'Purchase-7v5may5tl5bulonzl23ui4akcy-NONE',
+          Item: purchase,
+        })
+        .promise();
+
+      // Invoke the sendPurchaseEmail Lambda function
+      await lambda
+        .invoke({
+          FunctionName: `${process.env.SEND_EMAIL_LAMBDA_NAME!}`,
+          Payload: JSON.stringify({
+            email: session.customer_details?.email,
+            ...purchaseDetails,
+          }),
+        })
+        .promise();
+
+      // Update the purchase record to set emailSent to true
+      await dynamoDb
+        .update({
+          TableName: 'Purchase-7v5may5tl5bulonzl23ui4akcy-NONE',
+          Key: { id: paymentIntent.id },
+          UpdateExpression: 'set emailSent = :emailSent',
+          ExpressionAttributeValues: { ':emailSent': true },
+        })
+        .promise();
+    }
 
     return {
       statusCode: 200,
